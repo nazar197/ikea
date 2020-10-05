2c5b0fca9d0e6806b5078c08a5c64a7ba48c47d6
Nazar Pidhrushnyi <pidgrushnyi19@gmail.com>
2020-10-05 21:47:33 +0300

    feat: open submenu

--- script/main.js
+++ script/main.js
@@ -4,6 +4,9 @@ const btnBurger = document.querySelector('.btn-burger');
 const catalog = document.querySelector('.catalog');
 const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.btn-close');
+const catalogList = document.querySelector('.catalog-list');
+const subCatalog = document.querySelector('.subcatalog');
+const subCatalogHeader = document.querySelector('.subcatalog-header');
 
 const openMenu = () => {
   catalog.classList.add('open');
@@ -15,9 +18,19 @@ const closeMenu = () => {
   overlay.classList.remove('active');
 };
 
+const openSubMenu = event => {
+  event.preventDefault();
+  const catalogItemList = event.target.closest('.catalog-list__item');
+  if (catalogItemList) {
+    subCatalogHeader.innerHTML = catalogItemList.innerHTML;
+    subCatalog.classList.add('subopen');
+  };
+}
+
 btnBurger.addEventListener('click', openMenu);
 btnClose.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
+catalogList.addEventListener('click', openSubMenu);
 
 document.addEventListener('keydown', event => {
   if (event.code === 'Escape') {

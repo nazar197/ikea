1f68b4a70db49b48f7751eb7a81ff8ca8c155633
Nazar Pidhrushnyi <pidgrushnyi19@gmail.com>
2020-10-24 15:12:19 +0300

    feat:  add/remove goods to/from cart

--- script/index.js
+++ script/index.js
@@ -1,16 +1,15 @@
 'use strict';
 
-import './storage.js';
 import generateHeader from './generateHeader.js';
 import generateFooter from './generateFooter.js';
 import generateCatalog from './generateCatalog.js';
 import generateGoodsPage from './generateGoodsPage.js';
 import generateItemPage from './generateItemPage.js';
-import { loadData } from './loadData.js';
+import generateCartPage from './generateCartPage.js';
 
 generateHeader();
 generateFooter();
 generateCatalog();
 generateGoodsPage();
 generateItemPage();
-loadData();
+generateCartPage();
--- script/loadData.js
+++
@@ -1,9 +0,0 @@
-import { getData } from './getData.js';
-
-export const loadData = () => {
-  
-  if (location.pathname.includes('cart')) {
-    getData.cart(cartList, data => console.log(data));
-  }
-
-};
